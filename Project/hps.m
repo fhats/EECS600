416c3714ba772a298c8662a641ae843743ff951c
Fred Hatfull <fxh32@case.edu>
2011-05-02 11:17:06 -0400

    Semi-improved HPS scheme

--- Project/hps.m
+++ Project/hps.m
@@ -1,4 +1,4 @@
-function [ pitches ] = hps( w, Fs )
+function [ pitches ] = hps( w, Fs , stepthru )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
     
@@ -6,8 +6,8 @@ function [ pitches ] = hps( w, Fs )
     harmonics = 5;
     
     segment_size = 512;
-    
-    pitches = [];
+    smoothing_factor = 50; % a note should exist this many samples in either direction to 'smooth' the signal
+    pre_pitches = {};
     
     for i=1:segment_size/16:length(w) - segment_size,
         
@@ -24,16 +24,72 @@ function [ pitches ] = hps( w, Fs )
         end
         
         prod = exp(2 * d_s_sum);
-        
         frequency_range = 0:Fs/(length(power_spec)):Fs;
         
-        [max_val index] = max(prod);
+        %[max_val index] = max(prod);
+        %
+        %detected_pitch = frequency_range(index);
+        %
+        %pitches = [pitches, nearest_note(detected_pitch)];
         
-        detected_pitch = frequency_range(index);
+        max_val = max(prod);
+        pks = prod > (max_val * 0.25);
+        detected_pitches = [];
+        for p=1:length(pks),
+            if pks(p) > 0
+                detected_pitch = frequency_range(p);
+                detected_pitches = [detected_pitches nearest_note(detected_pitch)];
+            end
+        end
+        unique_pitches = unique(detected_pitches);
+        pre_pitches = [pre_pitches; unique_pitches];
         
-        pitches = [pitches, nearest_note(detected_pitch)];
+        if stepthru == 1
+            %figure(1)
+            plot(prod);
+            unique_pitches
+            pause
+        end
+    end
+
+    pitches = {};
+    % post-processing
+    for i=1:size(pre_pitches,1),
+        r_min = max(1,i-smoothing_factor);
+        r_max = min(size(pre_pitches,1), i+smoothing_factor);
+        ok_pitches = [];
+        for p=1:length(pre_pitches{i}),
+            pitch = pre_pitches{i}(p);
+            
+            pre = 1;
+            post = 1;
+            %check earlier
+            for r=r_min:i,
+                if isempty(find( pre_pitches{r} == pitch )), pre = 0; end
+            end
+            %check later
+            for r=i:r_max,
+                if isempty(find( pre_pitches{r} == pitch )), post = 0; end
+            end
+            
+            if pre == 1 || post == 1
+                ok_pitches = [ok_pitches pitch];
+            end
+            
+        end
+        pitches = [pitches; ok_pitches];
         
     end
 
+    % transform pitches into named notes
+    nn = {};
+    for p=1:size(pitches,1),
+        nnp = [];
+        for r=1:length(pitches{p}),
+            nnp = [nnp pitch2named_note(pitches{p}(r))];
+        end
+        nn = [nn; nnp];
+    end
+    pitches = nn;
 end
 
---
+++ Project/pitch2named_note.m
@@ -0,0 +1,51 @@
+function [ named_note ] = pitch2named_note( pitch )
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+    % constants used for this calculation
+    a = 2 ^ (1/12);         % we're using an equal-tempered scale
+    f0 = 440;               % typical base of 440 Hz
+    
+    % compute the number of half steps away from A4 the pitch is    
+    n = ( log10(pitch) - log10(f0) ) / log10(a);
+    n = round(n);
+
+    n_lett = 'A';
+    % Any music majors reading this: please don't kill me re: # vs. b
+    switch mod(n,12)
+        case 0
+            n_lett = 'A';
+        case 1
+            n_lett = 'A#';
+        case 2
+            n_lett = 'B';
+        case 3
+            n_lett = 'C';
+        case 4
+            n_lett = 'C#';
+        case 5
+            n_lett = 'D';
+        case 6
+            n_lett = 'D#';
+        case 7
+            n_lett = 'E';
+        case 8
+            n_lett = 'F';
+        case 9
+            n_lett = 'F#';
+        case 10
+            n_lett = 'G';
+        case 11
+            n_lett = 'G#';
+    end
+    
+    octave_distance = floor(n / 12);
+    if mod(n,12) > 2
+        octave_distance = octave_distance + 1;
+    end
+    octave = 4 + octave_distance;
+    
+    named_note = strcat(n_lett, int2str(octave));
+    
+end
+
